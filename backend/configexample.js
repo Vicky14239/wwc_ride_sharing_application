4d574a2d5ca7b369898fb0a759cc2af4fcdd4bc4
VICKY KUMAR <kv993495@gmail.com>
2025-01-15 10:11:22 +0530

    Update configexample.js

--- backend/configexample.js
+++ backend/configexample.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+    "pusher": {
+        appId: 'PUSHER_APP_ID',
+        key: 'PUSHER_APP_KEY',
+        secret: 'PUSHER_APP_SECRET',
+        cluster: 'PUSHER_APP_CLUSTER',
+        encrypted: true
+    },
+    "pusher-notifications": {
+        instanceId: 'YOUR_INSTANCE_ID_HERE',
+        secretKey: 'YOUR_SECRET_KEY_HERE'
+    }
+}
